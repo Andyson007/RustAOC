945b3f8c773ff91a0a80f08ff98684943a156caf
Andy <andreas.jan.van.der.meulen@gmail.com>
2025-12-11 00:36:27 +0100

    did stuff

--- 2025/day-10/src/bin/part2.rs
+++ 2025/day-10/src/bin/part2.rs
@@ -1,3 +1,4 @@
+#![feature(test)]
 use core::fmt::Debug;
 use core::iter;
 use std::collections::HashMap;
@@ -8,72 +9,75 @@ fn main() {
     println!("{ans}");
 }
 
-pub fn solve(input: &str) -> usize {
+pub fn solve(input: &str) -> u16 {
     input
         .lines()
-        .map(|line| {
-            let mut ws = line.split_whitespace();
-            // let len = ws.next().unwrap().len() - 2;
-            ws.next();
-            let buttons: Vec<_> = ws
-                .take_while(|x| !x.starts_with('{'))
-                .map(Button::parse)
-                .collect();
-            let grid = Required::parse(line.split_whitespace().last().unwrap());
-            solve_line(grid, &buttons)
-        })
+        .map(wrap_line)
         .inspect(|x| println!("{x}"))
         .sum()
 }
 
-pub fn solve_line(grid: Required, buttons: &[Button]) -> usize {
-    // println!("{grid:?}");
-    // println!("{buttons:?}");
+pub fn wrap_line(line: &str) -> u16 {
+    let mut ws = line.split_whitespace();
+    ws.next();
+    let buttons: Vec<_> = ws
+        .take_while(|x| !x.starts_with('{'))
+        .map(Button::parse)
+        .collect();
+    let grid = Required::parse(line.split_whitespace().last().unwrap());
+    solve_parsed_line(grid, &buttons)
+}
+
+pub fn solve_parsed_line(mut grid: Required, buttons: &[Button]) -> u16 {
     // returns all buttons which modify the current button and only future ones
-    let get_modifiers = |led| -> Vec<Vec<usize>> {
+    let get_modifiers = move |led| {
         buttons
             .iter()
-            .filter(|button| button.0.contains(&led))
-            .filter(|button| button.0.iter().all(|x| *x >= led))
-            .map(|x| x.0.clone())
+            .filter(move |button| button.0.contains(&led))
+            .filter(move |button| button.0.iter().all(|x| *x >= led))
             .collect()
     };
     let mut visited = HashMap::new();
-    solve_step(0, &grid, get_modifiers, &mut visited).unwrap()
+    let mut trivals = 0;
+    for i in 0..grid.0.len() {
+        if buttons.iter().filter(|x| x.0.contains(&i)).count() == 1 {
+            let b = buttons.iter().find(|x| x.0.contains(&i)).unwrap();
+            trivals += grid.0[i];
+            grid.sub(&b.0, grid.0[i]);
+        }
+    }
+    print!("{trivals}: ");
+    solve_step(0, &grid, get_modifiers, &mut visited).unwrap() + trivals
 }
 
-fn solve_step(
+fn solve_step<'a>(
     curr: usize,
     grid: &Required,
-    get_modifiers: impl Fn(usize) -> Vec<Vec<usize>> + Copy,
-    visited: &mut HashMap<Required, usize>,
-) -> Option<usize> {
+    get_modifiers: impl Fn(usize) -> Vec<&'a Button> + Copy,
+    visited: &mut HashMap<Required, u16>,
+) -> Option<u16> {
     if let Some(x) = visited.get(grid) {
         return Some(*x);
     }
     if grid.is_zero() {
         return Some(0);
     }
-    if grid.0[curr] == 0 {
-        return solve_step(curr + 1, grid, get_modifiers, visited);
+    if curr == grid.0.len() {
+        return None;
     }
-    let mut min = None;
     let available = &get_modifiers(curr);
-    'outer: for attempt in Combinations::new(available, grid.0[curr]) {
-        let mut grid = grid.clone();
-        for (b, amount) in attempt {
-            if !grid.sub(b, amount) {
-                continue 'outer;
+    Combinations::new(available.len(), grid.0[curr] as usize)
+        .filter_map(|combination| {
+            let mut grid = grid.clone();
+            for (b, amount) in available.iter().zip(combination) {
+                if !grid.sub(&b.0, amount) {
+                    return None;
+                }
             }
-        }
-        if let Some(s) = solve_step(curr + 1, &grid, get_modifiers, visited)
-            && min.is_none_or(|min| s < min)
-        {
-            visited.insert(grid, s);
-            min = Some(s)
-        }
-    }
-    min.map(|x| x + grid.0[curr])
+            solve_step(curr + 1, &grid, get_modifiers, visited)
+        })
+        .min()
+        .map(|x| x + grid.0[curr])
 }
 
 #[derive(Clone, PartialEq, Eq, Hash)]
@@ -92,7 +96,7 @@ impl Button {
 }
 
 #[derive(Clone, Hash, PartialEq, Eq)]
-pub struct Required(Vec<usize>);
+pub struct Required(Vec<u16>);
 
 impl Debug for Required {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -112,7 +116,7 @@ impl Required {
 
     /// Returns false if this would overpress a button
     /// true if everything is fine
-    pub fn sub(&mut self, button: &[usize], amount: usize) -> bool {
+    pub fn sub(&mut self, button: &[usize], amount: u16) -> bool {
         for b in button {
             if self.0[*b] < amount {
                 return false;
@@ -123,26 +127,23 @@ impl Required {
     }
 }
 
-pub struct Combinations<'a, T> {
-    arr: &'a [T],
-    lens: Vec<usize>,
+pub struct Combinations {
+    lens: Vec<u16>,
     needed: usize,
     done: bool,
 }
 
-impl<'a, T> Combinations<'a, T> {
-    pub fn new(arr: &'a [T], amount: usize) -> Self {
-        if arr.is_empty() {
+impl Combinations {
+    pub fn new(items: usize, amount: usize) -> Self {
+        if items == 0 {
             Self {
-                arr,
                 lens: Vec::new(),
                 needed: amount,
-                done: true,
+                done: false,
             }
         } else {
             Self {
-                arr,
-                lens: iter::repeat_n(0, arr.iter().len() - 1).collect(),
+                lens: iter::repeat_n(0, items - 1).collect(),
                 needed: amount,
                 done: false,
             }
@@ -150,38 +151,28 @@ impl<'a, T> Combinations<'a, T> {
     }
 }
 
-impl<'a, T> Iterator for Combinations<'a, T>
-where
-    T: Debug + Eq + Hash,
-{
-    type Item = HashMap<&'a T, usize>;
+impl Iterator for Combinations {
+    type Item = Vec<u16>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.done {
             return None;
         }
+        let clone = {
+            let mut clone = self.lens.clone();
+            let rest = self.needed as u16 - self.lens.iter().sum::<u16>();
+            clone.push(rest);
+            clone
+        };
+
         if self.lens.is_empty() {
             self.done = true;
-            return Some(HashMap::from([(&self.arr[0], self.needed)]));
+            return Some(clone);
         }
-        let rest = self.needed - self.lens.iter().sum::<usize>();
-        let ret = Some(
-            self.lens
-                .iter()
-                .enumerate()
-                .map(|(pos, amount)| (*amount, &self.arr[pos]))
-                .chain(iter::once((rest, self.arr.last().unwrap())))
-                .fold(
-                    HashMap::with_capacity(self.arr.len()),
-                    |mut acc, (amount, elem)| {
-                        *acc.entry(elem).or_default() += amount;
-                        acc
-                    },
-                ),
-        );
+        let ret = Some(clone);
         self.lens[0] += 1;
         let mut pos = 0;
-        while self.lens.iter().sum::<usize>() > self.needed {
+        while self.lens.iter().sum::<u16>() > self.needed as u16 {
             self.lens[pos] = 0;
             let Some(next) = self.lens.get_mut(pos + 1) else {
                 self.done = true;
@@ -196,8 +187,7 @@ where
 
 #[cfg(test)]
 mod test {
-
-    use crate::{Button, Required, solve, solve_line};
+    use crate::{Button, Required, solve, solve_parsed_line};
 
     #[test]
     fn example() {
@@ -220,7 +210,7 @@ mod test {
             Button(vec![0, 2]),
             Button(vec![0, 1]),
         ];
-        let ans = solve_line(req, &buttons);
+        let ans = solve_parsed_line(req, &buttons);
         assert_eq!(ans, 10)
     }
 
@@ -234,7 +224,7 @@ mod test {
             Button(vec![0, 1, 2]),
             Button(vec![1, 2, 3, 4]),
         ];
-        let ans = solve_line(req, &buttons);
+        let ans = solve_parsed_line(req, &buttons);
         assert_eq!(ans, 12)
     }
 
@@ -243,5 +233,44 @@ mod test {
         solve(
             "[.#.##.#.] (6,7) (0,2) (0,3,4,5,6,7) (0,1,2,3) (2) (1,2,4,5,7) (2,4,5,6,7) (0,1,3,5,7) {47,36,44,43,19,31,162,180}",
         );
+    }
+}
+
+#[cfg(test)]
+mod benches {
+    extern crate test;
+    use test::Bencher;
+
+    use crate::{solve, wrap_line};
+
+    #[bench]
+    // Only because it takes a reasonable amount of time
+    fn twelve_lines(b: &mut Bencher) {
+        b.iter(|| {
+            assert_eq!(
+                include_str!("../../input")
+                    .lines()
+                    .take(12)
+                    .map(wrap_line)
+                    .sum::<u16>(),
+                5
+            )
+        });
+    }
+
+    #[bench]
+    fn example_1(b: &mut Bencher) {
+        #[allow(clippy::iter_nth_zero)]
+        b.iter(|| solve(include_str!("../../input").lines().nth(0).unwrap()));
+    }
+
+    #[bench]
+    fn example_2(b: &mut Bencher) {
+        b.iter(|| solve(include_str!("../../input").lines().nth(2).unwrap()));
+    }
+
+    #[bench]
+    fn example_3(b: &mut Bencher) {
+        b.iter(|| solve(include_str!("../../input").lines().nth(2).unwrap()));
     }
 }
